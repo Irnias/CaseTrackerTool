e6305158802b405e343069ffb264d43088aba674
camila.ibar <camila.ibar@BA4GPL013775>
2017-05-29 16:58:41 -0300

    Modificacion ADD

--- CaseTrackerToolOutlookAddIn/NewCaseForm.vb
+++ CaseTrackerToolOutlookAddIn/NewCaseForm.vb
@@ -14,12 +14,10 @@ Public Class NewCaseForm
     Dim OutItem As Outlook.MailItem
     Dim OutReplyItem As Outlook.MailItem
     Dim conection As New OleDbConnection
-    Dim Comandos As New OleDbCommand
     Dim CaseID As Long
     Dim comands As New OleDbCommand
     Dim adapter As New OleDbDataAdapter
     Dim record As New DataSet
-    Dim consulta As String
     Public filePath As String
     Public filePath2 As String
 
@@ -29,15 +27,12 @@ Public Class NewCaseForm
         ResponsibleBox.Enabled = False
 
         'Load Teambox
-        TeamBox.Items.Add("BSI")
-        TeamBox.Items.Add("MAP")
-        TeamBox.Items.Add("BPC")
-        TeamBox.Items.Add("GT1")
-        TeamBox.Items.Add("MIS")
-        TeamBox.Items.Add("GFT")
+        LoadTeamBox()
+
         'Load ConectionBox
         ConectionBox.Items.Add("Office")
         ConectionBox.Items.Add("Home")
+
         'Load StatusBox
         StatusBox.Items.Add("Closed")
         StatusBox.Items.Add("Open")
@@ -107,9 +102,14 @@ Public Class NewCaseForm
         Me.Close()
     End Sub
 
-    Public Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ConectionBox.SelectedIndexChanged
-        Dim rows As Integer
-        Dim items As Object
+    Public Sub ConectionBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ConectionBox.SelectedIndexChanged
+        
+        'Restart conection if open
+        If conection.State = ConnectionState.Open Then
+            conection.Close()
+        End If
+
+        'Start new conection
         Try
             If ConectionBox.Text = "Office" Then
                 conection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = \\10.21.144.6\GBS Accenture Data\RTR\GA\MIS\Test1.accdb"
@@ -118,62 +118,13 @@ Public Class NewCaseForm
                 conection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = \\ramxilss002-f04.bp.com\ACNOPs\BA\Mariner\Mariner\RTR\MIS\Test1.accdb"
                 conection.Open()
             End If
-
         Catch ex As Exception
-            MsgBox("There is an error with the conections. Please report issue using the 'Notify issue' button", vbCritical)
-            GoTo Salir1
+            MsgBox(ex.Message)
         End Try
 
-
-
+        'Reaload Teambox
         TeamBox.Items.Clear()
-        Try
-            consulta = ("SELECT * FROM SubTeams ORDER BY ID DESC")
-            adapter = New OleDbDataAdapter(consulta, conection)
-            record = New DataSet
-            adapter.Fill(record, "SubTeams")
-            rows = record.Tables("SubTeams").Rows.Count
-            If rows <> 0 Then
-                DataGridView1.DataSource = record
-                DataGridView1.DataMember = "SubTeams"
-                items = (record.Tables("SubTeams").Rows(0).Item("ID"))
-                For x = 0 To items - 1
-                    TeamBox.Items.Add(record.Tables("SubTeams").Rows(x).Item("Team"))
-                Next
-            End If
-
-            TeamBox.Enabled = True
-
-
-        Catch ex As Exception
-            MsgBox("Error", vbCritical)
-            GoTo Salir1
-        End Try
-
-        'DESCOMENTAR ESTA PARTE SI ES NECESARIO
-
-        'filePath = "C:\Users\" & Environment.UserName & "\UserList.txt"
-
-        'Try
-        '    DateBox.Text = (DateTime.Now.ToString("MM/dd/yyyy"))
-
-        'Try
-
-        '    OutApp = CreateObject("Outlook.Application")
-        '    OutItem = OutApp.ActiveInspector.CurrentItem
-        '    EmailSender = OutItem.SenderName
-        '    RequestorBox.Text = EmailSender
-        '    Subject = OutItem.Subject
-        '    OriginalEmailTime = OutItem.ReceivedTime
-
-        '    ' 
-        '.Text = "Closed"
-
-        'Catch ex As Exception
-        '    MsgBox("Error when trying to get data from the current email item", vbCritical)
-        'End Try
-
-Salir1:
+        FillTeamBox()
 
     End Sub
 
@@ -181,62 +132,20 @@ Salir1:
         Dim rows As Integer
         Dim items As Object
 
+        'Reload Activities
         ActCategoryBox.Items.Clear()
-        Try
-            consulta = ("SELECT * FROM TeamsActivities WHERE SubTeam = '" & TeamBox.Text & " ' ORDER BY ID DESC")
-            adapter = New OleDbDataAdapter(consulta, conection)
-            record = New DataSet
-            adapter.Fill(record, "TeamsActivities")
-            rows = record.Tables("TeamsActivities").Rows.Count
-            If rows <> 0 Then
-                DataGridView1.DataSource = record
-                DataGridView1.DataMember = "TeamsActivities"
-                items = (record.Tables("TeamsActivities").Rows(0).Item("ID"))
-
-                For x = 0 To rows - 1
-                    ActCategoryBox.Items.Add(record.Tables("TeamsActivities").Rows(x).Item("Activity"))
-                Next
-            End If
-            ActCategoryBox.Enabled = True
-        Catch ex As Exception
-            MsgBox("Error en la parte del activity", vbCritical)
-            GoTo Salir2
-
-        End Try
-Salir2:
-
+        LoadTeamActivitiesBox()
     End Sub
 
     Private Sub ActCategoryBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ActCategoryBox.SelectedIndexChanged
         Dim rows As Integer
         Dim items As Object
 
+        'Reload Responsible Box
         ResponsibleBox.Items.Clear()
-        Try
-            consulta = ("SELECT * FROM TeamMembers ORDER BY ID DESC")
-            adapter = New OleDbDataAdapter(consulta, conection)
-            record = New DataSet
-            adapter.Fill(record, "TeamMembers")
-            rows = record.Tables("TeamMembers").Rows.Count
-            If rows <> 0 Then
-                DataGridView1.DataSource = record
-                DataGridView1.DataMember = "TeamMembers"
-                items = (record.Tables("TeamMembers").Rows(0).Item("ID"))
-
-                For x = 0 To items - 1
-                    ResponsibleBox.Items.Add(record.Tables("TeamMembers").Rows(x).Item("MemberEnterpriceID"))
-                Next
-            End If
-            ResponsibleBox.Enabled = True
-        Catch ex As Exception
-            MsgBox("Error en la parte del analyst", vbCritical)
-            GoTo Salir2
-        End Try
+        LoadResponsibleBox()
 
         DateBox.Text = (DateTime.Now.ToString("MM/dd/yyyy"))
-
-
-Salir2:
     End Sub
 
     Private Sub ResponsibleBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ResponsibleBox.SelectedIndexChanged
@@ -255,21 +164,19 @@ Salir2:
                 TicketNumberBox.Text = TeamBox.Text & TrakingID.Text
             End If
             TicketNumberBox.Visible = True
+
+            OutApp = CreateObject("Outlook.Application")
+            OutItem = OutApp.ActiveInspector.CurrentItem
+            EmailSender = OutItem.SenderName
+            RequestorBox.Text = EmailSender
+            Subject = OutItem.Subject
+            OriginalEmailTime = OutItem.ReceivedTime
+
+            rows = OutItem.id
         Catch ex As Exception
-            MsgBox("Error when trying to get the ticket number. Please contact the administrator", vbCritical)
-            GoTo Salir1
+            MsgBox(ex)
         End Try
 
-        OutApp = CreateObject("Outlook.Application")
-        OutItem = OutApp.ActiveInspector.CurrentItem
-        EmailSender = OutItem.SenderName
-        RequestorBox.Text = EmailSender
-        Subject = OutItem.Subject
-        OriginalEmailTime = OutItem.ReceivedTime
-
-        rows = OutItem.id
-
-Salir1:
     End Sub
 
     Private Function getNextTicketNumber() As Long
@@ -318,7 +225,7 @@ Salir1:
         If StatusBox.Text = "Closed" Then
             query = query & "'" & DateBox.Text & "',"
         Else
-            query = query & DBNull.Value
+            query = query & " " & DBNull.Value & ","
         End If
         'ActivityCategory = ActCategoryBox.Text
         query = query & "'" & ActCategoryBox.Text & "',"
@@ -340,4 +247,83 @@ Salir1:
         Return result
 
     End Function
+    
+    Private Sub LoadTeamBox()
+        Dim query As String = ""
+        Dim rows As Integer 
+        TeamBox.Enabled = False
+        
+        Try
+            query = ("SELECT * FROM SubTeams ORDER BY ID DESC")
+            adapter = New OleDbDataAdapter(consulta, conection)
+            record = New DataSet
+            adapter.Fill(record, "SubTeams")
+            rows = record.Tables("SubTeams").Rows.Count
+            If rows <> 0 Then
+                DataGridView1.DataSource = record
+                DataGridView1.DataMember = "SubTeams"
+                items = (record.Tables("SubTeams").Rows(0).Item("ID"))
+                For x = 0 To items - 1
+                    TeamBox.Items.Add(record.Tables("SubTeams").Rows(x).Item("Team"))
+                Next
+            End If
+            TeamBox.Enabled = True
+        
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub LoadTeamActivitiesBox()
+        Dim query As String = ""
+        Dim rows As Integer    
+        ActCategoryBox.Enabled = False
+
+        Try
+            query = ("SELECT * FROM TeamsActivities WHERE SubTeam = '" & TeamBox.Text & " ' ORDER BY ID DESC")
+            adapter = New OleDbDataAdapter(consulta, conection)
+            record = New DataSet
+            adapter.Fill(record, "TeamsActivities")
+            rows = record.Tables("TeamsActivities").Rows.Count
+            If rows <> 0 Then
+                DataGridView1.DataSource = record
+                DataGridView1.DataMember = "TeamsActivities"
+                items = (record.Tables("TeamsActivities").Rows(0).Item("ID"))
+
+                For x = 0 To rows - 1
+                    ActCategoryBox.Items.Add(record.Tables("TeamsActivities").Rows(x).Item("Activity"))
+                Next
+            End If
+            ActCategoryBox.Enabled = True
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
+
+    Private Sub LoadResponsibleBox()
+        Dim query As String = ""
+        Dim rows As Integer 
+        ResponsibleBox.Enabled = False
+        
+        Try
+            query = ("SELECT * FROM TeamMembers ORDER BY ID DESC")
+            adapter = New OleDbDataAdapter(consulta, conection)
+            record = New DataSet
+            adapter.Fill(record, "TeamMembers")
+            rows = record.Tables("TeamMembers").Rows.Count
+            If rows <> 0 Then
+                DataGridView1.DataSource = record
+                DataGridView1.DataMember = "TeamMembers"
+                items = (record.Tables("TeamMembers").Rows(0).Item("ID"))
+
+                For x = 0 To items - 1
+                    ResponsibleBox.Items.Add(record.Tables("TeamMembers").Rows(x).Item("MemberEnterpriceID"))
+                Next
+            End If
+            ResponsibleBox.Enabled = True
+        
+        Catch ex As Exception
+            MsgBox(ex)
+        End Try
+    End Sub
 End Class
